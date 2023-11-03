301e4189e05a7058ec5c7c6d53cf02ae20aaef9b
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2023-11-03 20:26:56 -0300

    Add arquivos iniciais

---
+++ EncurtadorDeURL/src/main/java/br/com/life/EncurtadorDeURL/EncurtadorDeUrlApplication.java
@@ -0,0 +1,13 @@
+package br.com.life.EncurtadorDeURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorDeUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorDeUrlApplication.class, args);
+	}
+
+}
---
+++ EncurtadorDeURL/src/test/java/br/com/life/EncurtadorDeURL/EncurtadorDeUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.life.EncurtadorDeURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorDeUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
