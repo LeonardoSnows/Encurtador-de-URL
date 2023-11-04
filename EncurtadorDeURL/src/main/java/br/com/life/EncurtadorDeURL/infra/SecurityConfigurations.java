ac03746ea89252f0bccd5be80f035e5fc755ee8e
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2023-11-03 21:20:14 -0300

    Add Arquivos de Infra par configuração de segurança
    arquivos que irão auxiliar nas configurações de negocio

---
+++ EncurtadorDeURL/src/main/java/br/com/life/EncurtadorDeURL/infra/SecurityConfigurations.java
@@ -0,0 +1,10 @@
+package br.com.life.EncurtadorDeURL.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Service;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+}
